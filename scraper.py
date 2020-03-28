8fe49ee05ce2dd4894e017712dc9716bc60f5ed6
J-T-H-N <34819086+J-T-H-N@users.noreply.github.com>
2020-03-28 15:40:16 -0400

    Update scraper.py
    Edits need to be made on class_finder

--- scraper.py
+++ scraper.py
@@ -8,17 +8,18 @@ import pandas as pd
         
 
 def main():
-    site = 'https://www.classifiedads.com/search.php?keywords=&cid=15&lid=rx8008&lname=Seattle%2C+WA&from=c'
-    siteList = (javascript_finder()) + (page_navi()) + (css_finder()) + (img_finder() + (php_finder()))
-    siteList, extraList = getPageLink(siteList, site) #function sorts through duplicates, external links, javascript:void(0) --- it is worth checking to see if any of these are important and should be scanned
-    
+    site = 'https://www.classifiedads.com/search.php?cid=326&lid=rx8008&lname=Seattle%2C+WA&from=s&keywords='
+    siteList = (class_finder())
+    #siteList, extraList = getPageLink(siteList, site) #function sorts through duplicates, external links, javascript:void(0) --- it is worth checking to see if any of these are important and should be scanned
+    #extraList = [x.decode("utf-8") for x in extraList]
+
     f = open('links.txt', "w")
     f.write('\n'.join(siteList))
-    f.write('\n'.join(extraList))
+    #f.write('\n'.join(extraList))
     f.close()
 
 def simple_get():
-    url = 'https://www.classifiedads.com/search.php?keywords=&cid=15&lid=rx8008&lname=Seattle%2C+WA&from=c'
+    url = 'https://www.classifiedads.com/search.php?cid=326&lid=rx8008&lname=Seattle%2C+WA&from=s&keywords='
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -28,6 +29,14 @@ def simple_get():
     req = urllib.request.Request(url, headers=hdr)
     response = urllib.request.urlopen(req)
     return response
+
+
+def class_finder():
+    site = simple_get()
+    soup = BeautifulSoup(site, "html.parser")
+    results = soup.find_all('a',{"class": ''})
+    
+    print(results)
 
 
 def javascript_finder():
@@ -61,20 +70,10 @@ def css_finder():
     l = [x.encode('utf-8') for x in l]
     return l
 
-def php_finder():
-    site = simple_get()
-    soup = BeautifulSoup(site, "html.parser")
-    l = [i.get('href') for i in soup.find_all('/link.php') if i .get('href')]
-    l = [x.encode('utf-8') for x in l]
-    return l
 
 def getPageLink(siteList, site):
     siteList = list(set(siteList))
     extraList = []
-    for item in siteList:
-        if "php" in str(item):
-            extraList.append(item)
-            siteList.remove(item)
     for item in siteList:
         if "http" in str(item):
             extraList.append(item)
@@ -89,7 +88,7 @@ def getPageLink(siteList, site):
         if item == "#":
             siteList.remove(item)
 
-    siteList = [str(site) + str(s) for s in siteList]
+    siteList = [str(s) for s in siteList]
     return siteList, extraList
 
-main()
+class_finder()
